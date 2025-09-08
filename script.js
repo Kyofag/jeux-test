a44f1f98edff1f2dcef5d804227c8ce80af08583
Fabian <fabian.libert@eduhainaut.be>
2025-09-08 15:54:11 +0200

    test

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function create() {
     platforms = this.physics.add.staticGroup();
 
     // La plateforme du sol
-    const floor = platforms.create(400, 568, 'platform').setScale(4, 1).refreshBody();
+    const floor = platforms.create(400, 568, 'platform').setScale(4, 0.2).refreshBody();
 
     // Redimensionne la hitbox pour qu'elle corresponde à la plateforme
     floor.body.setSize(floor.displayWidth, floor.displayHeight);
